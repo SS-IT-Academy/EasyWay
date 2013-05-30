c3606389634e12135ef566e9ec8d317cd42f427e
shyc <serhiy.yushkevych@gmail.com>
2013-05-30 12:22:20 +0300

    Recreate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '283b729a9d0cc6d6c34512e95c7cddb6d9160e57cfd42d58ccf866e0ab066a74a512f925111ec68ed76a1b831848dc75946dade443c624cf05748d6850205acb'

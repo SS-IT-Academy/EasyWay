8fa3622bebc770860df5c3bf2b1d22fe396ec294
svisigor <svisigor@mail.ru>
2013-06-17 22:33:36 +0300

    an error fixed in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => "admin@softserve.ua"
   def welcome_email(user)
-  @user = user
-  @url = "<a href="http://localhost:3000/sign_in">http://localhost:3000/sign_in</a>"
-  @site_name = "localhost"
-  mail(:to => user.email, :subject => "Welcome to as.")
-end
+    @user = user
+    @url = "<a href='http://localhost:3000/sign_in'>http://localhost:3000/sign_in</a>"
+    @site_name = "localhost"
+    mail(:to => user.email, :subject => "Welcome to as.")
+  end
 end

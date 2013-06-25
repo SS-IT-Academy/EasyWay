233fe2b153138d4d56595a8beeee99e4eb9bb3c0
Artur Sarkisyan <go.ar2r@gmail.com>
2013-06-25 22:42:30 +0300

    change scaffold css with style css

--- app/models/table_filter.rb
+++ app/models/table_filter.rb
@@ -1,13 +1,15 @@
 class TableFilter < ActiveRecord::Base
   attr_accessible :delimiter, :position_float, :position_num, :resource_id, :table_template_id
   
+  POSITION_FLOATS = ["next", "left", "right"]
+  DELIMITERS = [", ", "; "," ", "<br />"]
   belongs_to :table_template
     #:foreign_key => "table_template_id"
   belongs_to :resource
     #:foreign_key => "resource_id"
   validates :delimiter, :presence => false
   validates :position_float, :presence => false
-  validates :position_num, :presence => true
+  validates :position_num, :presence => true,  :numericality => true
   validates :resource_id, :presence => true
   validates :table_template_id, :presence => true
 end

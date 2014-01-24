2904d3cab0dea2ab18a39d266a6a7805ab57af6b
vagrant <vagrant@precise32.(none)>
2014-01-24 15:45:06 +0400

    add resources related to factories

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,10 +1,28 @@
 FactoryGirl.define do
+  factory :resource_type do
+    sequence(:name) { |n| "new resource type##{n}" } 
+  end
 
-	factory :resource do
+  factory :field_type do
+    name "new field type"
+  end
 
-		description "Lesson"
-		resource_type_id 2
+  factory :resource do
+    description "new resource"
+    resource_type
+  end
 
-	end
+  factory :field do
+    name "new field"
+    field_type
+    resource_type
+    sequence(:resource_type_reference_id) { |n| n }
+  end
 
+  factory :resource_value do
+    field
+    resource
+    value "new resource value"
+    sequence(:resource_reference_id) { |n| n }
+  end
 end

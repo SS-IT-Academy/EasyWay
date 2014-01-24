84dc86a4cdf4495fa89d52c2ab5ba0d4e7f7d65e
Lebediev <alex-lebedev@hotmail.com>
2014-01-24 10:48:56 +0200

    add tests for notify controller

--- spec/factories/notify_events.rb
+++ spec/factories/notify_events.rb
@@ -1,11 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :notify_event do
-    event_id 1
-    name "MyString"
-    observer_id 1
-    template_id 1
-    event_id 1
+  factory :notify_event do |f|
+    f.name "MyString"   
+    f.notify_template_id 1
+    f.event_id 1
+    f.notify_observer_id 2
+    #f.recipients "first"
   end
 end
--- spec/factories/notify_schedulers.rb
+++ spec/factories/notify_schedulers.rb
@@ -1,7 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :notify_observer do
-    name "MyString"
+  factory :notify_scheduler do |f|
+    f.period 1
+    f.start_at 10.22
+    f.end_at 12.22
+
   end
 end

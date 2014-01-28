5681d89ade8f4c3c03f9dbb469572f68b60db68d
Ihor Strumetsky <llighterr@gmail.com>
2014-01-28 10:37:30 +0200

    Add relations between Field and Validator

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,6 +2,9 @@ class Field < ActiveRecord::Base
   attr_accessible :field_type_id, :name, :resource_type_id, :resource_type_reference_id
   
   has_many :resource_values, :dependent => :destroy
+  has_many :field_validations
+  has_many :validators, through: :field_validations
+
   belongs_to :field_type  
   belongs_to :resource_type
   
--- app/models/validator.rb
+++ app/models/validator.rb
@@ -1,5 +1,7 @@
 class Validator < ActiveRecord::Base
   attr_accessible :body, :field_type_id, :name
-  
+
   belongs_to :field_type
+  has_many :field_validations
+  has_many :fields, through: :field_validations
 end

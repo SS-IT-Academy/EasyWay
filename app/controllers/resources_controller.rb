e74fca18cebad6b643111e151a79071407d6f214
Ihor Strumetsky <llighterr@gmail.com>
2014-01-31 10:43:05 +0200

    Change resources show method

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -29,10 +29,14 @@ class ResourcesController < ApplicationController
   def show
     @resource = Resource.find(params[:id])
     @resource_type = ResourceType.find(@resource.resource_type_id)
-    @values = ResourceValue.where("resource_id =?", params[:id])
+    @values = ResourceValue.where("resource_id = ?", params[:id])
     @field_types = []
     @values.each do |value|
-      @field_types << Field.find(value.field_id)
+      if Field.find(value.field_id).field_type_id == 7
+        @field_types << ResourceType.find(Resource.find(value.resource_reference_id).resource_type_id)
+      else
+        @field_types << FieldType.find(Field.find(value.field_id).field_type_id)
+      end
     end     
     
     respond_to do |format|
--- app/models/resource_value.rb
+++ app/models/resource_value.rb
@@ -3,8 +3,8 @@ class ResourceValue < ActiveRecord::Base
   
   belongs_to :resource
   belongs_to :field
-  # belongs_to :resource_type, :through => :resource,
-  #   :foreign_key => "resource_reference_id"
+  belongs_to :resource_type, :through => :resource,
+     :foreign_key => "resource_reference_id"
   delegate :resource_type, :to => :resource, :allow_nil => true
   validates :field_id, :resource_id, :presence => true
 end

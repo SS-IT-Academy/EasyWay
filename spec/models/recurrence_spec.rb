f5b90d750883eca76f84b78d4a3e0c08c1ef85c2
gra1 <bo4ylia@gmail.com>
2014-02-02 23:14:59 +0200

    modify some model spec

--- spec/models/recurrence_spec.rb
+++ spec/models/recurrence_spec.rb
@@ -4,40 +4,51 @@ require 'spec_helper'
 
 describe Recurrence do
 
-  # context "shoudla gem" do
+  context "shoudla gem" do
 
-  #   it { should validate_presence_of(:end_date) }
-  #   it { should validate_presence_of(:start_date) }
-  #   it { should have_many(:events) }
-
-  # end
-
-	context "Checking validations" do
-
-		it 'should have valid factory' do
-			build(:recurrence).should be_valid
-		end
-
-		it 'is invalid without some attribute' do 
-			build(:invalid_recurrence).should_not be_valid
-		end
-
-	end
-
-	context "accessible attributes" do
-    it "should not allow access to ..." do
-      expect{
-        Recurrence.new(la_la_la: true)
-        Recurrence.new(tu_tu_tu: true)
-      }.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    context "checks association" do      
+      it { should have_many(:events) }
     end
 
-    it "should allow access to attr_accessible" do
-      expect{
-        Recurrence.new(repetition: true)
-        Recurrence.new(end_date: true)
-        Recurrence.new(start_date: true)
-      }.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    context "checking validations" do
+      it { should validate_presence_of(:end_date) }
+      it { should validate_presence_of(:start_date) }
     end
+
+    context "checks attr_accessible" do
+      it { should allow_mass_assignment_of(:repetition) }
+      it { should allow_mass_assignment_of(:end_date) }
+      it { should allow_mass_assignment_of(:start_date) }
+    end
+
   end
+
+	# context "Checking validations" do
+
+	# 	it 'should have valid factory' do
+	# 		build(:recurrence).should be_valid
+	# 	end
+
+	# 	it 'is invalid without some attribute' do 
+	# 		build(:invalid_recurrence).should_not be_valid
+	# 	end
+
+	# end
+
+	# context "accessible attributes" do
+ #    it "should not allow access to ..." do
+ #      expect{
+ #        Recurrence.new(la_la_la: true)
+ #        Recurrence.new(tu_tu_tu: true)
+ #      }.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+ #    end
+
+ #    it "should allow access to attr_accessible" do
+ #      expect{
+ #        Recurrence.new(repetition: true)
+ #        Recurrence.new(end_date: true)
+ #        Recurrence.new(start_date: true)
+ #      }.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+ #    end
+ #  end
 end

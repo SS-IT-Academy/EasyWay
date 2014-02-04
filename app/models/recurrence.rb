fd8bc4ed0c31d2eaf6d0cc4251bb2d86719886db
gra1 <bo4ylia@gmail.com>
2014-02-04 22:10:29 +0200

    modify recurrence model & event form

--- app/models/recurrence.rb
+++ app/models/recurrence.rb
@@ -10,10 +10,10 @@ class Recurrence < ActiveRecord::Base
   serialize :repetition, Hash
 
   def repetition=(new_rec)
-	  if RecurringSelect.is_valid_rule?(new_rec)
-	    write_attribute(:repetition, RecurringSelect.dirty_hash_to_rule(new_rec).to_hash)
-	  else
+	  if new_rec == "{}"
 	    write_attribute(:repetition, nil)
+	  else	    
+      write_attribute(:repetition, RecurringSelect.dirty_hash_to_rule(new_rec).to_hash)
 	  end
 	end
 

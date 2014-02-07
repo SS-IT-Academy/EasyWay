144e7b5c28d05599aeba5e02d51bc2c90e4dfad7
hvozdulych <hvozdulych@gmail.com>
2014-02-07 14:30:15 +0200

    changed smtp settings for mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,16 @@ EasyW::Application.configure do
   config.action_mailer.perform_deliveries = true,
   config.action_mailer.raise_delivery_errors = true,
   config.action_mailer.delivery_method = :smtp,
-  config.action_mailer.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => 'gmail.com',
-  :user_name => 'lv105ror@gmail.com',
-  :password => '123qwASD',
-  :authentication => 'plain',
-  :enable_starttls_auto => true } 
+  
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+
+  # config.action_mailer.smtp_settings = {
+  # :address => "smtp.gmail.com",
+  # :port => 587,
+  # :domain => 'gmail.com',
+  # :user_name => 'lv105ror@gmail.com',
+  # :password => '123qwASD',
+  # :authentication => 'plain',
+  # :enable_starttls_auto => true } 
 end

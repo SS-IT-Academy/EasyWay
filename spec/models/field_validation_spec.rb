0939d280ff34af344ced01274e83579770d85d0e
Ihor Strumetsky <llighterr@gmail.com>
2014-02-11 07:40:49 +0200

    Add tests and factories to Validator, FieldValidation models

--- spec/models/field_validation_spec.rb
+++ spec/models/field_validation_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe FieldValidation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:field_validation) { create :field_validation }
+  
+  subject { field_validation }
+
+  it { should respond_to(:field_id) }
+  it { should respond_to(:validator_id) }
+  
 end
+

6f752de7e179b95eec69bf6b5f7e7d7f1b9861ba
gra1 <bo4ylia@gmail.com>
2014-02-17 12:45:52 +0200

    add datepicker

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,6 +53,11 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(params[:event])
 
+    date_start_at = params[:date_start_at][0].to_time
+    hour_start_at = params[:date][:hour].to_i
+    minute_start_at = params[:date][:minute].to_i
+    @event.start_at = date_start_at + hour_start_at.hour + minute_start_at.minutes
+    
     days_duration = params[:days_duration].to_i
     hours_duration = params[:hours_duration].to_i
     minutes_duration = params[:minutes_duration].to_i
@@ -105,6 +110,11 @@ class EventsController < ApplicationController
   # PUT /events/1.json
   def update
     @event = Event.find(params[:id])
+
+    date_start_at = params[:date_start_at][0].to_time
+    hour_start_at = params[:date][:hour].to_i
+    minute_start_at = params[:date][:minute].to_i
+    @event.start_at = date_start_at + hour_start_at.hour + minute_start_at.minutes
     
     days_duration = params[:days_duration].to_i
     hours_duration = params[:hours_duration].to_i

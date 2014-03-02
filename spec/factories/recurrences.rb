abda35a127de86922cd2a2a68313bfdb5857e80f
gra1 <bo4ylia@gmail.com>
2014-03-02 20:35:33 +0200

    modify spec

--- spec/factories/recurrences.rb
+++ spec/factories/recurrences.rb
@@ -4,14 +4,10 @@ FactoryGirl.define do
 
   factory :recurrence do
     repetition "2"
-    start_date DateTime.now.utc
-    end_date DateTime.now.utc + 1.day
   end
 
   factory :invalid_recurrence, parent: :recurrence do
     repetition ""
-    start_date ""
-    end_date ""
   end
 
 end

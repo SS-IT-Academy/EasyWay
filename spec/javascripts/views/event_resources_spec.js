07f51cdebd36c5b13e5934ae954455c8b1b1a902
gra1 <bo4ylia@gmail.com>
2014-03-23 21:57:51 +0200

    add jasmine test

--- spec/javascripts/views/event_resources_spec.js
+++ spec/javascripts/views/event_resources_spec.js
@@ -1,7 +1,32 @@
 describe('EasyWay.Views.EventResource', function(){
-  it('not null tag',function(){
+
+  it('call render method in model view',function(){
     var eventResource = new EasyWay.Models.EventResource();
     var eventResourceView = new EasyWay.Views.EventResource({model: eventResource});
-    expect(eventResourceView.render().$el.find('option')).not.toBeNull();
+    spyOn(eventResourceView, 'render');
+    eventResourceView.render();
+    expect(eventResourceView.render).toHaveBeenCalled();
   });
+
+});
+
+describe('Easy.Views.EventResource', function(){
+
+  beforeEach(function(){
+    this.eventResource  = new EasyWay.Collections.EventResource();
+    this.eventResourseCollectionView = new EasyWay.Views.EventResources({collection: eventResourseCollection});
+  });
+
+  it('call render method in collection view',function(){  
+    spyOn(this.eventResourseCollectionView, 'render');
+    this.eventResourseCollectionView.render();
+    expect(this.eventResourseCollectionView.render).toHaveBeenCalled();
+  });
+
+  it('call addOne method in collection view',function(){
+    spyOn(this.eventResourseCollectionView, 'addOne');
+    this.eventResourseCollectionView.addOne();
+    expect(this.eventResourseCollectionView.addOne).toHaveBeenCalled();
+  });
+
 });
